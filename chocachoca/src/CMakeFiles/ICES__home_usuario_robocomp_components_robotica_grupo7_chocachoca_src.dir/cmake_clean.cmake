d47c5675821936c8f09e2bbaa1d29f2c5dce3c3d
David <92219223+DavidBP02@users.noreply.github.com>
2024-10-09 16:35:23 +0200

    Delete chocachoca directory

--- chocachoca/src/CMakeFiles/ICES__home_usuario_robocomp_components_robotica_grupo7_chocachoca_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_usuario_robocomp_components_robotica_grupo7_chocachoca_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()

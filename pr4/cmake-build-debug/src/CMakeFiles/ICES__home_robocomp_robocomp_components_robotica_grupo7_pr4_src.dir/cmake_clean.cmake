4607a1e9589a48e7db8097bd8d8da2a44f1a7f91
521337 <57076905+521337@users.noreply.github.com>
2025-01-27 19:10:20 +0100

    Delete pr4 directory

--- pr4/cmake-build-debug/src/CMakeFiles/ICES__home_robocomp_robocomp_components_robotica_grupo7_pr4_src.dir/cmake_clean.cmake
+++
@@ -1,5 +0,0 @@
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/ICES__home_robocomp_robocomp_components_robotica_grupo7_pr4_src.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- pr4/cmake-build-debug/src/CMakeFiles/ICES__home_robocomp_robocomp_components_robotica_grupo7_pr4_src.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty custom commands generated dependencies file for ICES__home_robocomp_robocomp_components_robotica_grupo7_pr4_src.
-# This may be replaced when dependencies are built.
